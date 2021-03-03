19008173f400e55b831b0b24393ecca95046eb25
Guillermo Saavedra <guillermo.saavedra@elektra.com.mx>
2021-03-03 10:55:06 -0600

    [ADD] User is login validation

--- SMNEApp/View/ViewControllerTabBar.swift
+++ SMNEApp/View/ViewControllerTabBar.swift
@@ -15,5 +15,18 @@ class ViewControllerTabBar: UITabBarController {
         // Do any additional setup after loading the view.
         tabBar.unselectedItemTintColor = .C00D9CC()
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let user = UserDefaults.standard
+        if let email = user.string(forKey: "email") {
+            
+        } else {
+            let storyboard = UIStoryboard(name: "LogIn", bundle: nil)
 
+            let initialViewController = storyboard.instantiateViewController(withIdentifier: "initial")
+            initialViewController.modalPresentationStyle = .fullScreen
+            self.present(initialViewController, animated: false, completion: nil)
+        }
+    }
 }
